fc87be2963cd2137b3cb316493cc08da94452634
MyHome_PC <abdulrhmanalaa352@gmail.com>
2024-11-12 18:29:45 +0200

    deleted old file

--- Embedded_C/Lecture_2/Assignment/Versatilepb.Brd_UART/app.c
+++
@@ -1,8 +0,0 @@
-#include <uart.h>
-
-unsigned char string[32] = "learn-in-depth : <Abdulrhman>";
-
-void main(void)
-{
-	uart_send_data(string);
-}
--- Embedded_C/Lecture_2/Assignment/Versatilepb.Brd_UART/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-#define UART0DR     *((volatile unsigned int*)((unsigned int*)0x101F1000))
-
-extern void uart_send_data(unsigned char* p_tx);
-
-#endif
