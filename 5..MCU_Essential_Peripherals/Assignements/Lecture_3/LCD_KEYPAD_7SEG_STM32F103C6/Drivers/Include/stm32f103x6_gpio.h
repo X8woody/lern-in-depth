f6f7413cd2d24caa0973600675d00868c92628ba
OWNER/LEADER <abdulrhmanalaa352@gmail.com>
2025-05-26 13:52:21 +0300

    add new assignment with drivers file

---
+++ 5..MCU_Essential_Peripherals/Assignements/Lecture_3/LCD_KEYPAD_7SEG_STM32F103C6/Drivers/Include/stm32f103x6_gpio.h
@@ -0,0 +1,124 @@
+/*
+ * stm32f103x6_gpio.h
+ *
+ *  Created on: May 18, 2025
+ *      Author: Abdulrhman
+ */
+
+#ifndef INCLUDE_STM32F103X6_GPIO_H_
+#define INCLUDE_STM32F103X6_GPIO_H_
+//-----------------------------
+//Includes
+//-----------------------------
+
+#include "stm32f103x6.h"
+
+//-----------------------------
+
+
+//=================================================================================================
+//-----------------------------
+//User type definitions
+//-----------------------------
+
+typedef enum
+{
+	MCAL_ERROR = 0u,
+	MCAL_OK
+}MCAL_STATUS_enum;
+//--------------------------------------------------------------------------
+// @ref MCAL_GPIO_PIN_STATUS typedef enumeration.
+typedef enum
+{
+	GPIO_PIN_RESET = 0u,
+	GPIO_PIN_SET
+}MCAL_GPIO_PIN_STATUS;
+//--------------------------------------------------------------------------
+typedef enum
+{
+	INPUT_MODE = 0u,
+	OUTPUT_MODE
+}MCAL_PIN_MODE_ENUM;
+//--------------------------------------------------------------------------
+//CNFy[1:0]: Port x configuration bits (y= 0 .. 7)
+typedef enum
+{
+	Analog_mode     = 0b0000,
+	Floating_input  = 0b0100,
+	Input_PULL_UD   = 0b1000,
+	Reserved_1      = 0b1100
+}MCAL_CNFy_INPUT_PIN_MODE_ENUM;
+//--------------------------------------------------------------------------
+//CNFy[1:0]: Port x configuration bits (y= 0 .. 7)
+typedef enum
+{
+	GPOutput_Push_pull  = 0b0000,
+	GPOutput_OpenDarain = 0b0100,
+	AFOutput_Push_pull  = 0b1000,
+	AFOutput_OpenDarain = 0b1100
+}MCAL_CNFy_OUTPUT_PIN_MODE_ENUM;
+//--------------------------------------------------------------------------
+//MODEy[1:0]: Port x mode bits (y= 0 .. 7)
+typedef enum
+{
+	Input_mode                  = 0b0000,
+	Output_mode_max_speed_10MHz = 0b0001,
+	Output_mode_max_speed_2MHz  = 0b0010,
+	Output_mode_max_speed_50MHz = 0b0011
+}MCAL_MODEy_PIN_MODE_ENUM;
+//--------------------------------------------------------------------------
+
+// @ref PIN_CFG_TypeDef.
+typedef struct
+{
+	volatile uint16_t                 PIN_NUMBER;
+	MCAL_PIN_MODE_ENUM               __PinMode;
+	MCAL_CNFy_INPUT_PIN_MODE_ENUM    CNF_INPUT_CASE;
+	MCAL_CNFy_OUTPUT_PIN_MODE_ENUM   CNF_OUTPUT_CASE;
+	MCAL_MODEy_PIN_MODE_ENUM         MODE_CASE;
+}PIN_CFG_TypeDef;
+//-------------------------------------------------------------------------
+//=================================================================================================
+//-----------------------------
+//Macros Configuration References
+//-----------------------------
+#define GPIO_PIN_0                                        (uint16_t)0x0001U   //(1 << 0);
+#define GPIO_PIN_1                                        (uint16_t)0x0002U   //(1 << 1);
+#define GPIO_PIN_2                                        (uint16_t)0x0004U   //(1 << 2);
+#define GPIO_PIN_3                                        (uint16_t)0x0008U   //(1 << 3);
+#define GPIO_PIN_4                                        (uint16_t)0x0010U   //(1 << 4);
+#define GPIO_PIN_5                                        (uint16_t)0x0020U   //(1 << 5);
+#define GPIO_PIN_6                                        (uint16_t)0x0040U   //(1 << 6);
+#define GPIO_PIN_7                                        (uint16_t)0x0080U   //(1 << 7);
+#define GPIO_PIN_8                                        (uint16_t)0x0100U   //(1 << 8);
+#define GPIO_PIN_9                                        (uint16_t)0x0200U   //(1 << 9);
+#define GPIO_PIN_10                                       (uint16_t)0x0400U   //(1 << 10);
+#define GPIO_PIN_11                                       (uint16_t)0x0800U   //(1 << 11);
+#define GPIO_PIN_12                                       (uint16_t)0x1000U   //(1 << 12);
+#define GPIO_PIN_13                                       (uint16_t)0x2000U   //(1 << 13);
+#define GPIO_PIN_14                                       (uint16_t)0x4000U   //(1 << 14);
+#define GPIO_PIN_15                                       (uint16_t)0x8000U   //(1 << 15);
+
+#define GPIO_PIN_MASK                                     0xFFFFU
+/*
+ * @ref Module_REF_NAME_define
+*/
+//=================================================================================================
+/*
+* ===============================================
+* APIs Supported by "MCAL GPIO DRIVER"
+* ===============================================
+*/
+
+void MCAL_GPIO_INIT(GPIOx_REG_typedef *GPIOx, PIN_CFG_TypeDef *GPIO_PIN_CFG);
+void MCAL_GPIO_SET_DataDire(GPIOx_REG_typedef *GPIOx, MCAL_PIN_MODE_ENUM Dir, uint16_t PinNum);
+void MCAL_GPIO_WRITE_PIN(GPIOx_REG_typedef *GPIOx, uint16_t PinNum, MCAL_GPIO_PIN_STATUS PinStatus);
+void MCAL_GPIO_WRITE_PORT(GPIOx_REG_typedef *GPIOx, uint16_t SpecPins);
+void MCAL_GPIO_TOGGLE_PIN(GPIOx_REG_typedef *GPIOx, uint16_t PinNum);
+
+MCAL_GPIO_PIN_STATUS MCAL_GPIO_READ_PIN(GPIOx_REG_typedef *GPIOx, uint16_t PinNum);
+uint16_t MCAL_GPIO_READ_PORT(GPIOx_REG_typedef *GPIOx);
+
+MCAL_STATUS_enum MCAL_GPIO_PIN_LOCK(GPIOx_REG_typedef *GPIOx, uint16_t PinNum);
+
+#endif /* INCLUDE_STM32F103X6_GPIO_H_ */
---
+++ 5..MCU_Essential_Peripherals/Assignements/Lecture_3/LCD_KEYPAD_7SEG_STM32F103C6/Drivers/Keypad/keypad.c
@@ -0,0 +1,177 @@
+/*
+ * Keypad.c
+ *
+ * Created: 4/29/2025 5:04:02 PM
+ *  Author: Abdulrhman
+ */ 
+//-----------------------------
+//Includes
+#include "keypad.h"
+
+//-----------------------------
+//GLOBAL VARIABLE DECLARATION
+//-----------------------------
+KEYPAD_PINS KEY_PAD;
+//-----------------------------
+#if MCU_DVICE == __STM32F103C6
+
+void delay_ms(uint32_t __ms)
+{
+	uint32_t i,j;
+	for(i = 0; i < __ms; i++)
+		for(j = 0; j < 255; j++);
+}
+#endif
+
+
+/*----------------------API's_decalarations--------------------*/
+//--------------SET Columns and Rows of KEYPAD and initiate its pins.-------------------//
+void HAL_KEY_INIT(volatile DataType_Pins R1, volatile DataType_Pins R2, volatile DataType_Pins R3, volatile DataType_Pins R4, volatile DataType_Pins C1, volatile DataType_Pins C2, volatile DataType_Pins C3, volatile DataType_Pins C4){
+	//KEY_PIN_I_O &= ~((1 << C1) | (1 << C2) | (1 << C3) | (1 << C4));
+	//KEY_PIN_I_O |= ((1 << R1) | (1 << R2) | (1 << R3) | (1 << R4));
+	{
+		KEY_PAD.K_R1 = (1 << R1);
+		KEY_PAD.K_R2 = (1 << R2);
+		KEY_PAD.K_R3 = (1 << R3);
+		KEY_PAD.K_R4 = (1 << R4);
+	}
+	{
+		KEY_PAD.K_C1 = (1 << C1);
+		KEY_PAD.K_C2 = (1 << C2);
+		KEY_PAD.K_C3 = (1 << C3);
+		KEY_PAD.K_C4 = (1 << C4);
+	}
+	PIN_CFG_TypeDef GPIO_PIN_CFG;
+	GPIO_PIN_CFG.PIN_NUMBER 	 = (KEY_PAD.K_R1 | KEY_PAD.K_R2 | KEY_PAD.K_R3 | KEY_PAD.K_R4);
+	GPIO_PIN_CFG.__PinMode 		 = OUTPUT_MODE;
+	#if MCU_DVICE == __STM32F103C6
+		GPIO_PIN_CFG.CNF_OUTPUT_CASE = GPOutput_OpenDarain;
+		GPIO_PIN_CFG.MODE_CASE       = Output_mode_max_speed_10MHz;
+	#endif
+	MCAL_GPIO_INIT(KEYPAD_GPIO_DATA, &GPIO_PIN_CFG);
+
+	GPIO_PIN_CFG.PIN_NUMBER 	 = (KEY_PAD.K_C1 | KEY_PAD.K_C2 | KEY_PAD.K_C3 | KEY_PAD.K_C4);
+	GPIO_PIN_CFG.__PinMode 		 = INPUT_MODE;
+	#if MCU_DVICE == __STM32F103C6
+		GPIO_PIN_CFG.CNF_INPUT_CASE  = Floating_input;
+		GPIO_PIN_CFG.MODE_CASE       = Input_mode;
+	#endif
+	MCAL_GPIO_INIT(KEYPAD_GPIO_DATA, &GPIO_PIN_CFG);
+	#if MCU_DVICE == __ATmega32
+		MCAL_GPIO_WRITE_PORT(KEYPAD_GPIO_DATA, 0xFF);
+	#endif
+	//KEY_PORT = 0xFF;
+}
+//--------------Read inputs from Keypad.-------------------//
+unsigned char HAL_KEY_READ_DATA(){
+	volatile DataType_Pins* P_KEYPAD_R = &KEY_PAD.K_R1;
+	volatile DataType_Pins* P_KEYPAD_C = &KEY_PAD.K_C1;
+	uint8_t i,j;	
+	for(i = 0; i < 4; i++)
+	{
+		#if MCU_DVICE == __ATmega32
+			#define LOGIC_1					GPIO_PIN_SET
+			#define LOGIC_2					GPIO_PIN_RESET
+			#define CHECK_PIN_LOGIC()		!(MCAL_GPIO_READ_PIN(KEYPAD_GPIO_DATA, *P_KEYPAD_C))
+		#else
+			#define LOGIC_1					GPIO_PIN_RESET
+			#define LOGIC_2					GPIO_PIN_SET
+			#define CHECK_PIN_LOGIC()		(MCAL_GPIO_READ_PIN(KEYPAD_GPIO_DATA, *P_KEYPAD_C))
+		#endif
+		MCAL_GPIO_WRITE_PIN(KEYPAD_GPIO_DATA, ((KEY_PAD.K_R1) | (KEY_PAD.K_R2) | (KEY_PAD.K_R3) | (KEY_PAD.K_R4)), LOGIC_1);
+		MCAL_GPIO_WRITE_PIN(KEYPAD_GPIO_DATA, *P_KEYPAD_R, LOGIC_2);
+		#if MCU_DVICE == __STM32F103C6
+			delay_ms(100);
+		#endif
+		for(j = 0; j < 4; j++)
+		{					
+			if(CHECK_PIN_LOGIC())
+			{ 
+ 				while(CHECK_PIN_LOGIC());
+				switch (j)
+				{
+					case 0:
+						if(i == 0)
+						{
+							return '7';
+						}
+						else if(i == 1)
+						{
+							return '4';
+						}
+						else if(i == 2)
+						{
+							return '1';
+						}
+						else if(i == 3)
+						{
+							return 'N';
+						}
+						break;
+					case 1:
+						if(i == 0)
+						{
+							return '8';
+						}
+						else if(i == 1)
+						{
+							return '5';
+						}
+						else if(i == 2)
+						{
+							return '2';
+						}
+						else if(i == 3)
+						{
+							return '0';
+						}
+						break;
+					case 2:
+						if(i == 0)
+						{
+							return '9';
+						}
+						else if(i == 1)
+						{
+							return '6';
+						}
+						else if(i == 2)
+						{
+							return '3';
+						}
+						else if(i == 3)
+						{
+							return '=';
+						}
+							break;
+					case 3:
+						if(i == 0)
+						{
+							return '/';
+						}
+						else if(i == 1)
+						{
+							return '*';
+						}
+						else if(i == 2)
+						{
+							return '-';
+						}
+						else if(i == 3)
+						{
+							return '+';
+						}
+						break;					
+				}
+			}
+			else
+			{
+			}
+			P_KEYPAD_C++;
+		}
+		P_KEYPAD_R++;
+		P_KEYPAD_C = &KEY_PAD.K_C1;		
+	}
+	return 'A';
+}
+//-------------------------------------------------------------------------------------------
